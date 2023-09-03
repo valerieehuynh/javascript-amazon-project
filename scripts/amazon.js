1252a6ff8e9e9e43bd8958124c45e80e54910fa0
valerieehuynh <vlhuynh2@uci.edu>
2023-09-03 14:34:01 -0700

    converted html containers into javascript

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,28 +24,30 @@ const products = [{
   priceCents: 799
 }];
 
+let productsHTML = '';
+
 products.forEach((product) => {
-  const html =  `
+  productsHTML += `
   <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
-          src="images/products/athletic-cotton-socks-6-pairs.jpg">
+          src="${product.image}">
       </div>
 
       <div class="product-name limit-text-to-2-lines">
-        Black and Gray Athletic Cotton Socks - 6 Pairs
+      ${product.name}
       </div>
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-45.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
-          87
+          ${product.rating.count}
         </div>
       </div>
 
       <div class="product-price">
-        $10.90
+        $${(product.priceCents / 100).toFixed(2)}
       </div>
 
       <div class="product-quantity-container">
@@ -75,4 +77,8 @@ products.forEach((product) => {
       </button>
   </div>
   `;
-})
+});
+
+console.log(productsHTML);
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
